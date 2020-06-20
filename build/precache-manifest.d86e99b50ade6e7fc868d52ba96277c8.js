21616a696bbe0ef81ee36a28c6c0d49d23f5c8b3
Atharva Taranekar <taranekaratharva@gmail.com>
2020-06-20 23:02:40 +0530

    check

---
+++ build/precache-manifest.d86e99b50ade6e7fc868d52ba96277c8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f0e35d9a8978748c4a44dd085a1254",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36580bb032ff9fa22390",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "636c6a353835c7290a09",
+    "url": "/static/css/main.55d15629.chunk.css"
+  },
+  {
+    "revision": "36580bb032ff9fa22390",
+    "url": "/static/js/2.70ad7339.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "/static/js/2.70ad7339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "636c6a353835c7290a09",
+    "url": "/static/js/main.a5f8c23a.chunk.js"
+  },
+  {
+    "revision": "63ac20d4153ea2544c8d",
+    "url": "/static/js/runtime-main.4b25e468.js"
+  },
+  {
+    "revision": "8bf3876c4796da23c54f2623c65ea9c7",
+    "url": "/static/media/IMG-20200528-WA0011.8bf3876c.jpg"
+  },
+  {
+    "revision": "0f8662853ebd6cdebdc127b213c5bb22",
+    "url": "/static/media/bg1.0f866285.png"
+  },
+  {
+    "revision": "e059e1076d1506aa658c0798f5eeb0f4",
+    "url": "/static/media/bg2.e059e107.png"
+  },
+  {
+    "revision": "55a5893a7668d2568141423b824134a9",
+    "url": "/static/media/bg3.55a5893a.png"
+  },
+  {
+    "revision": "6e1b39ddff393c420bc8ef09e425305e",
+    "url": "/static/media/laptop-guitar.6e1b39dd.png"
+  }
+]);
